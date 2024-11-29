37642a3bcf9ec7c9b9d0d1e616f3f4e84c4cec9c
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-29 13:57:29 +0800

    test(arch): added more arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,3 +2,48 @@
 
 arch()->preset()->php();
 arch()->preset()->laravel();
+
+arch('strict types')
+    ->expect([
+        'App\Models',
+        'App\Controllers'
+    ])
+    ->toUseStrictTypes();
+
+arch('avoid open for extension')
+    ->expect([
+        'App\Models',
+        'App\Controllers'
+    ])
+    ->classes()
+    ->toBeFinal()
+    ->ignoring([]);
+
+arch('ensure no extends')
+    ->expect('App')
+    ->classes()
+    ->not->toBeAbstract()
+    ->ignoring([]);
+
+
+arch('avoid inheritance')
+    ->expect('App')
+    ->classes()
+    ->toExtendNothing()
+    ->ignoring([
+        'App\Console\Commands',
+        'App\Exceptions',
+        'App\Http\Requests',
+        'App\Jobs',
+        'App\Livewire',
+        'App\Mail',
+        'App\Models',
+        'App\Notifications',
+        'App\Providers',
+        'App\View',
+    ]);
+
+arch('annotations')
+    ->expect('App')
+    ->toHavePropertiesDocumented()
+    ->toHaveMethodsDocumented();
