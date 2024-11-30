eedb363b42f3a97757cd3d097c3a6d62aee46d21
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-30 16:03:23 +0800

    test: removed all unit testsuite

--- tests/Unit/Models/CategoryTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Models\Category;
-
-it('returns expected keys', function () {
-    $category = Category::factory()->make();
-
-    expect($category->getAttributes())->toHaveKeys(['name', 'description']);
-});
--- tests/Unit/Models/IngredientTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Models\Ingredient;
-
-it('returns expected keys', function () {
-    $ingredient = Ingredient::factory()->make();
-
-    expect($ingredient->getAttributes())->toHaveKeys(['name']);
-});
